b8831a92bb0c6d65f841b074a310af4639704d65
Eurico Araujo <eurico.oneiros@gmail.com>
2022-03-02 14:07:42 -0300

    teste

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -6,13 +6,14 @@ resource "aws_s3_bucket" "datalake-eurico-atv1"{
     CURSO = "EDC"
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+
 
 }
 
+#  server_side_encryption_configuration {
+#    rule {
+#      apply_server_side_encryption_by_default {
+#        sse_algorithm = "AES256"
+#      }
+#    }
+#  }
