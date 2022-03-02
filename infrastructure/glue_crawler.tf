9ddb8f77bfad0d7ca30fe46edcb4a3592a88f33f
Eurico Araujo <eurico.oneiros@gmail.com>
2022-03-02 10:03:00 -0300

    implementação da esteira de deploy github

---
+++ infrastructure/glue_crawler.tf
@@ -0,0 +1,22 @@
+resource "aws_glue_crawler" "atividade1" {
+  database_name = aws_glue_catalog_database.atividade1.name
+  name          = "atividade1_crawler"
+  role          = aws_iam_role.glue_role.arn
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.atividade1.bucket}"
+  }
+
+  configuration = <<EOF
+{
+   "Version": 1.0,
+   "Grouping": {
+      "TableGroupingPolicy": "CombineCompatibleSchemas" }
+}
+EOF
+
+  tags = {
+    IES   = "IGTI",
+    CURSO = "EDC"
+  }
+}
