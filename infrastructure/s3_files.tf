6fc971b85b99abc6d64f84511df43afd84d9f592
Eurico Araujo <eurico.oneiros@gmail.com>
2022-03-02 21:27:29 -0300

    criado upload do arquivo .csv para o buclet principal

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -6,3 +6,11 @@ resource "aws_s3_bucket_object" "parquet_insert" {
     etag = filemd5("../etl/1_parquet_insert.py")  
 }
 
+
+resource "aws_s3_bucket_object" "arquivos_csv_enem_2020" {
+    bucket = aws_s3_bucket.datalake-eurico-atv1.id
+    key = "raw-data/enem2020.csv"
+    acl = "private"
+    source = "../data/DADOS/MICRODADOS_ENEM_2020.csv"
+    etag = filemd5("../data/DADOS/MICRODADOS_ENEM_2020.csv")  
+}
